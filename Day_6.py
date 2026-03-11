d2aae9bffb68c0839ffef4d4a6004264dac8bd9f
Neha kumari <nehadubey09908@gmail.com>
2026-03-11 22:39:13 +0530

    commit

---
+++ Day_6.py
@@ -0,0 +1,102 @@
+# Day: 6
+
+# Operator => it can perform any specific task between two operands
+
+# a + b => a and b are operands and + is operator
+# here we can see + operates Addition operation.
+
+# Types
+# 1. Arithmetic Operators
+# 2. Assignment Operators
+# 3. Comparison Operators
+# 4. Logical Operators
+# 5. Bitwise Operators
+# 6. Identity Operators
+# 7. Membership Operators
+
+# 1. Arithmetic Operators:
+# + , - , * , / ,//,  % , ** 
+
+# + => Addition
+# - => Subtraction
+# * => Multiplication
+
+# / => Division  => it gives us float value, it gives us Quotient value.
+
+# // => Floor Division  => it gives us integer value, it gives us Quotient value.
+
+# % => Modulus  => it gives us remainder value.
+
+# ** => Exponentiation  => it gives us power value.
+
+
+
+
+
+# ## Data Types:
+# int => 10, 20, 30....
+# float => 10.5, 20.5, 30.5....
+# string => "Hello", "World", "Ravi"
+# boolean => True, False
+
+# —----------------------------------------------------------------------------------------------------- 
+x = 10
+y = 3
+print("the output of x + y : ", x + y)  # Addition
+print("the output of x - y : ", x - y)  # Subtraction
+print("the output of x * y : ", x * y)  # Multiplication
+print("Hii, Ravi, You can see the value of x and y : ")  
+print("hii, this is Zeyarat")
+
+print("the output of division : ", x / y)  # Division
+print("the output of floor division : ", x // y)  # Floor Division
+print("the output of modulus : ", x % y)  # Modulus
+
+print("the output of exponentiation : ", 2 ** 4)  # Exponentiation              
+# —------------------------------------ Github —------------------------------------------------------------
+# —------TASK —------
+# What is Github and what is git .
+# How to install git bash locally.
+# How to create a github account 
+# what ‘s the difference between git and gihub
+# Benefits of github and git
+
+# ### How to setup Git and Github ?
+
+# Step: Git download locally  , and setup following this video https://youtu.be/t2-l3WvWvqg?si=onv6-lvbbFrqGLSs
+# Setup Github , and create Username and password ( credential)
+# Open cmd , and check for version 
+# => git -- version
+# => git config --global user.name "enter your username"
+# git config --global user.email "enter your email"
+
+
+# git config --global user.email
+# git config --global user.name
+
+# 10 . Create a new folder on desktop 
+
+# And double click on that folder and at the top where path is showing 
+
+# , we delete the folder path and write down cmd
+# And press enter button 
+# When it opens in cmd , we have to write (“code .“)
+
+# , now this folder can open in VS code .
+
+# Step: 1=> firstly we have to create a file , and write some lines of code 
+# Step 2 => click on NEW terminal 
+# Step 2.1 => open github and create new REPOSETRIES , and provide a name , and follow the commands 
+# Step 3 => git init  (“connect folder with github”)
+# Step 4 => git add . ( “add all files to commit ”) 
+# (“for single file commit ,we use git add nav.html”)
+# Step 5 => git status (“we can see status like which files are committed on git”)
+# Step 6 => git commit -m “first commit”
+# Step 7 => rest of commands 
+
+# —--------------------------------------------------------------
+# ## How to add new  file in our existed repo?
+
+# Step: 1=> git add “file name”  OR git add .
+# Step 2 => git commit -m “your message”
+# Step 3 => git push -u origin main
