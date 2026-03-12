efeacc5cb569cbfe99e937be5ec8aa95cce23d5a
Nehadubey143 <nehadubey09908@gmail.com>
2026-03-12 12:33:13 +0530

    What is String and Example

---
+++ Day_7.py
@@ -0,0 +1,49 @@
+'''String :-
+
+=> it is a sequence of characters. 
+
+
+it is enclosed with Single , Double or Triple Quotation marks.
+
+
+'Neha'  => single Quotation mark
+
+"Neha's Watch"  => Double Quotation mark'''
+
+'''it's a fantastic topic "Hostel Life". ''' #=> Tiple Quotation mark
+
+my_name = "Neha"
+email = "nehadubey23@gmail.com"
+print(my_name)
+
+print(email)
+
+# -------------------------------------------------- 
+
+
+my_name = "neha"
+email = "nehadubey23@gmail.com"
+
+
+print("name : ", my_name)
+
+print("email : ", email)
+
+
+# string replication :- 
+
+my_name = "Neha"
+
+print(my_name * 12)
+
+
+# string Concatenation :-
+# => it is used to add two or more than two string . 
+
+first_name = "Neha"
+second_name = "Dubey"
+
+full_name = first_name + second_name
+
+print("Fullname is : ", full_name )
+print(first_name, second_name)
