993b5574f34f4f9e7c5539c7a2e39193084edcb9
Nehadubey143 <nehadubey09908@gmail.com>
2026-04-01 22:11:45 +0530

    What is list and some Example

---
+++ Day_11.py
@@ -0,0 +1,26 @@
+# () = Parentheses
+# {} = curly braces 
+# [] = Square Bracket 
+
+# my_list = [ 33, 44,55, 56]
+
+# my_list = [22, "first", True, 6.6]
+
+# List => it can store multiple type of data. 
+# => it is mutable .
+
+# List Indexing 
+
+
+my_list = [22, "first", True, 6.6]
+my_list = [22, "first", True, 6.6]
+
+
+my_list = [ "first ", "rohit", "Palak"]
+
+
+
+
+
+
+print("Inactive")
