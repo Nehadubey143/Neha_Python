bf1ac0605f2b113013197cc040a749aa009da4d8
Nehadubey143 <nehadubey09908@gmail.com>
2026-04-10 23:19:20 +0530

    Input Function

---
+++ Day_20.py
@@ -0,0 +1,22 @@
+# input() => it is used to take data from user side . 
+
+# => by default it gives us string value , 
+# that's why we have to specify the datatype . 
+
+
+name = "neha"
+print("YOur name is : ", name )
+
+# -------------------------------------------------
+
+
+# name = input("enter your good name ")
+
+# print("YOur name is : ", name )
+
+# -------------------------------------- 
+
+
+age = int(input("enter your age "))
+
+print("YOur age is : ", age )
